3bb3d6362f9462343c0eab3bb2ebd28102b11ec1
Venkata Sai Pavan Gandham <77606826+PavanGandham@users.noreply.github.com>
2022-04-05 10:42:05 +0530

    added new jenkins install script

--- jenkins.sh
+++ jenkins.sh
@@ -1,14 +1,53 @@
 #!/bin/bash
-sudo apt update && sudo apt install openjdk-8-jdk -y
-sudo java -version
+
+# Step 1 — Installing Jenkins
+
+# First, add the repository key to the system
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list'
-sudo apt-get update
-sudo apt-get install jenkins -y
+
+# When the key is added, the system will return OK. 
+# Next, append the Debian package repository address to the server’s sources.list:
+# echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+# Ensure that your software packages are up to date on your instance
+sudo apt update -y
+
+# Install Java JDK 8  
+sudo apt install openjdk-8-jdk -y
+
+# Check Java Version
+sudo java -version
+
+# Install Jenkins and its dependencies
+sudo apt install jenkins -y
+
+# Step 2 — Starting Jenkins
+
+# Stating Jenkins Server
+sudo systemctl start jenkins
+
+# Verify the Status of Jenkins Server
+sudo systemctl status jenkins
+
+# Enable Jenkins server to start at boot
+sudo systemctl enable jenkins
+
+# Step 3 — Opening the Firewall
+
+# Open Port 8080 on ufw for jenkins 
+sudo ufw allow 8080
+
+# check the ufw satus 
+sudo ufw status
+
+# If the ufw is inactive the below commands allows openssh and enable ufw
+sudo ufw allow OpenSSH
+sudo ufw enable
 
 #---------------------------------High-Availability----------------------------------------------------------------
 
-reloadconfig user API Token - 110de73dcec71840cded4f643cd3b7ebac
+# reloadconfig user API Token - 110de73dcec71840cded4f643cd3b7ebac
 
 #Download Jenkins CLI on both Primary and Secondary Servers (If loadbalancing used else secondary is enough)
 wget http://ec2-3-238-93-44.compute-1.amazonaws.com:8080/jnlpJars/jenkins-cli.jar
