ef1144c393156caaf311eb07f4851a79762dc793
Venkata Sai Pavan Gandham <77606826+PavanGandham@users.noreply.github.com>
2022-04-16 09:09:54 +0000

    added nexus ubuntu install script in testing stage.

---
+++ Nexus_ubuntu_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+################################################################################################
+#Script Name :Nexus_ubuntu_install.sh
+#Description : Install the Nexus artifactory for storing the project artifacts and dependencies.
+#Author :Pavan Gandham
+#Email :pavangandham99@gmail.com
+#License : GNU GPL-3
+################################################################################################
+
+echo "Installing Sonatype Nexus artifactory in $(uname)"
+
