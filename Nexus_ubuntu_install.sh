939565bd40ea6372735f020a8907edc045cd5880
Pavan-Gandham <pavangandham99@gmail.com>
2022-04-17 19:52:16 +0530

    nexus install script in progress

--- Nexus_ubuntu_install.sh
+++ Nexus_ubuntu_install.sh
@@ -10,6 +10,12 @@
 
 OS_NAME="$(sudo cat /etc/os-release | grep -i pretty_name | cut -d = -f2)"
 PRE_REQ="1.OpenJDK-8 2.Minimum-CPUs:4 3.User-sudo-privileges 4.Set-User-limits 5.Web-Browser 6.Firewall/Inbound-port:22,8081"
+USER_NAME="nexus"
+EXIST_USER="$(cat /etc/passwd | grep -i $USER_NAME | cut -d : -f1)"
+JAVA_VERSION="$(cat version.txt | grep -i version | grep -Eo '[0-9]\.[0-9]+')"
+MAX_CPU="4"
+CPU_CORES="$(cat /proc/cpuinfo | grep -i "cpu cores" | cut -d : -f2)"
+
 echo -e "Installing Sonatype Nexus artifactory in $OS_NAME\n"
 sleep 1
 read -ra arr <<<$PRE_REQ
@@ -21,24 +27,49 @@ done
 # Verifying Java JDk is available or not
 func_jdk() {
     echo -e "\n####### Step-1 : Checking if Java JDK version 1.8 is available or not #######\n"
-    java -version >version.txt 2>&1
-    JAVA_VERSION="$(cat version.txt | grep -i version | grep -Eo '[0-9]\.[0-9]+')"
+    sudo java -version >version.txt 2>&1
     if [ $JAVA_VERSION == "1.8" ]; then
         echo -e "Java version $JAVA_VERSION is available\n"
     else
         sudo apt install openjdk-8-jre-headless -y
+        echo -e "Java 8 is successfully installed"
+        sudo java -version
     fi
 }
 
 # Verifying CPU is min 4 or not
 func_cpu() {
-    MAX_CPU="4"
-    CPU_CORES="$(cat /proc/cpuinfo | grep -i "cpu cores" | cut -d : -f2)"
     echo -e "\n####### Step-2 : Checking if Having CPUs = 4 available or not #######\n"
     if [ $CPU_CORES == "4" ]; then
         echo -e "\n Minimum CPU requirement is met\n"
     else
         echo -e "Minimum CPU requirement is not met , need $(expr $MAX_CPU - $CPU_CORES) more CPUs\n"
+        printf "Hint : Select the instance type as t2/t3.medium or large in AWS"
+    fi
+}
+
+# Verifying user had sudo previleges or not
+
+func_sudo() {
+    echo -e "Checking if user $USER_NAME exists or not.........\n"
+    sleep 1
+    if [ $EXIST_USER -eq "nexus" ]; then
+        echo "$USER_NAME exists"
+        echo -e "Checking if user $USER_NAME is having sudo access or not\n"
+    else
+        echo -e "No user named $USER_NAME in the users."
+        printf "As security practice, not to run nexus service using root user,  
+            So, Creating a new user $USER_NAME for running Nexus service\n"
+        echo -e "Provide the Password and details for the new user(details is not mandatory)\n"
+        sudo adduser $USER_NAME
+    fi
+}
+
+# Setting up the New User Limits
+
+func_ulimits() {
+    if [ $EXIST_USER -eq $USER_NAME ]; then
+        
     fi
 }
 
