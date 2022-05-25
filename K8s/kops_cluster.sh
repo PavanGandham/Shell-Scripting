0f36ff8dcdeac4476651a0b19b8ea6c0390cbdd3
Venkata Sai Pavan Gandham <77606826+PavanGandham@users.noreply.github.com>
2022-05-25 14:20:01 +0530

    Added remaining script

--- K8s/kops_cluster.sh
+++ K8s/kops_cluster.sh
@@ -10,9 +10,22 @@ complete -F __start_kubectl ku' >> ~/.bashrc
 
 source ~/.bashrc
 
+# Create an SSH Key pair 
+ssh-keygen -t rsa -b 4096
 
+# Creating Cluster using kops
 kops create cluster --name=pavank8s.ml \
 --state=s3://pavank8sb12cluster --zones=us-east-1a,us-east-1b,us-east-1c \
 --node-count=3 --master-count=1 --node-size=t3.medium --master-size=t3.medium \
 --master-zones=us-east-1a --master-volume-size 10 --node-volume-size 10 \
 --dns-zone=pavank8s.ml --yes
+
+# Getting cluster name
+kops get cluster --show-labels
+
+# Validate cluster 
+kops validate cluster --wait 10m
+
+# Delete Cluster
+#kops delete cluster --name pavank8s.ml --yes
+
