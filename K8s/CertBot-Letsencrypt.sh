7e8254c449555fd2a1216f0c31cd724b21a81817
Venkata Sai Pavan Gandham <77606826+PavanGandham@users.noreply.github.com>
2022-06-11 15:48:48 +0530

    Made some changes in CertBot file

--- K8s/CertBot-Letsencrypt.sh
+++ K8s/CertBot-Letsencrypt.sh
@@ -7,7 +7,7 @@ sudo snap install --classic certbot
 cd /snap/bin/
 cp certbot /usr/local/bin/
 ./certbot  -h
-./certbot certonly --manual --preferred-challenges=dns --email mavrick202@gmail.com \
---server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.trainingk8s.xyz
+./certbot certonly --manual --preferred-challenges=dns --email pavangandham99@gmail.com \
+--server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.pavank8s.ml
 #Executing above command will give you a a hostname and txt value to create in the DNS/Route53. Create the record and give the TTL value as 1 min.
 #Wait for few min and give Yes at the prompt .
